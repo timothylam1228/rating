4171f29352fcf15ca8d6f2a4d40aa1ed0d414da9
timothylam1228 <timothylam1228@gmail.com>
2024-04-27 03:14:12 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -34,16 +34,14 @@ function App() {
       person_generation: prompt.PersonGeneration,
       aspect_ratio: prompt.AspectRatio,
     };
-    console.log(data);
-
-    const response = await fetch("https://essaa-creatolens-cdr-media-service-sit-y7nazd37ga-df.a.run.app/gen-image/v1/prompt", {
-      method: "POST",
-      // headers: {
-      //   "Content-Type": "application/json",
-      //   // Add any other headers you need, like authorization headers
-      // },
-      body: JSON.stringify(data),
-    });
+    console.log("data", data);
+    const response = await fetch(
+      "https://essaa-creatolens-cdr-media-service-sit-y7nazd37ga-df.a.run.app/gen-image/v1/prompt",
+      {
+        method: "POST",
+        body: JSON.stringify(data),
+      }
+    );
 
     const image = await response.json();
     setImageSource(image.data);
@@ -66,32 +64,27 @@ function App() {
   const onRate = async (event) => {
     setLoading(true);
     const value = event.target.value;
-    const result = { ...prompt, Rating: value, Image: imageSource };
+    const result = { ...prompt, Rating: value };
+    // const result = { "Prompt": prompt.Prompt };
     var form_data = new FormData();
     for (var key in result) {
       form_data.append(key, result[key]);
     }
 
-    const bodyData = JSON.stringify(result);
-
 
     // upload image to google drive
 
-    fetch(
-      "https://script.google.com/macros/s/AKfycbxEOWTsVsNxb_dm0t56Vm3jaVvatbp3GhKsFhKRkoOru0Di2VVayUS0c3WIDaB3FnhkWw/exec",
-      {
-        method: "POST",
-        body: bodyData,
-      }
-    )
+    fetch(import.meta.env.VITE_APP_SCRIPT, {
+      method: "POST",
+      body: form_data,
+    })
       .then((res) => res.json())
       .then((data) => {
         console.log("data", data);
       })
       .catch((error) => console.log("error", error));
     setLoading(false);
-    setImageSource("");
-
+    // setImageSource("");
   };
 
   return (
@@ -114,64 +107,63 @@ function App() {
           disabled={loading}
           name="Prompt"
         ></input>
-        <button disabled={loading} onClick={onSubmitPromt}>submit</button>
+        <button disabled={loading} onClick={onSubmitPromt}>
+          submit
+        </button>
       </div>
-
 
       <div>
         Aspect Ratio
-        <div>
-          <label>
-            <input
-              type="radio"
-              name="AspectRatio"
-              value="1:1"
-              checked={prompt.AspectRatio === "1:1"}
-              onChange={handleChange}
-            />
-            1:1
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="AspectRatio"
-              value="9:16"
-              checked={prompt.AspectRatio === "9:16"}
-              onChange={handleChange}
-            />
-            9:16
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="AspectRatio"
-              value="16:9"
-              checked={prompt.AspectRatio === "16:9"}
-              onChange={handleChange}
-            />
-            16:9
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="AspectRatio"
-              value="4:3"
-              checked={prompt.AspectRatio === "4:3"}
-              onChange={handleChange}
-            />
-            4:3
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="AspectRatio"
-              value="3:4"
-              checked={prompt.AspectRatio === "3:4"}
-              onChange={handleChange}
-            />
-            3:4
-          </label>
-        </div>
+        <label>
+          <input
+            type="radio"
+            name="AspectRatio"
+            value="1:1"
+            checked={prompt.AspectRatio === "1:1"}
+            onChange={handleChange}
+          />
+          1:1
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="AspectRatio"
+            value="9:16"
+            checked={prompt.AspectRatio === "9:16"}
+            onChange={handleChange}
+          />
+          9:16
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="AspectRatio"
+            value="16:9"
+            checked={prompt.AspectRatio === "16:9"}
+            onChange={handleChange}
+          />
+          16:9
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="AspectRatio"
+            value="4:3"
+            checked={prompt.AspectRatio === "4:3"}
+            onChange={handleChange}
+          />
+          4:3
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="AspectRatio"
+            value="3:4"
+            checked={prompt.AspectRatio === "3:4"}
+            onChange={handleChange}
+          />
+          3:4
+        </label>
       </div>
       <div>
         Language:
@@ -182,7 +174,6 @@ function App() {
           checked={prompt.Language === "en"}
           onChange={handleChange}
           placeholder="Language"
-          
         />
         English
         <input
@@ -202,50 +193,56 @@ function App() {
           value=""
           checked={prompt.Persona === ""}
           defaultChecked
-          onChange={handleChange} />
+          onChange={handleChange}
+        />
         <label>None</label>
         <input
           type="radio"
           name="Persona"
           value="Foodie"
           checked={prompt.Persona === "Foodie"}
-          onChange={handleChange} />
+          onChange={handleChange}
+        />
         <label>Foodie</label>
         <input
           type="radio"
           name="Persona"
           value="Digital Marketer"
           checked={prompt.Persona === "Digital Marketer"}
-          onChange={handleChange} />
+          onChange={handleChange}
+        />
         <label>Digital Marketer</label>
-
         <input
           type="radio"
           name="Persona"
           value="Content Manager"
           checked={prompt.Persona === "Content Manager"}
-          onChange={handleChange} />
+          onChange={handleChange}
+        />
         <label>Content Manager</label>
         <input
           type="radio"
           name="Persona"
           value="Photographer"
           checked={prompt.Persona === "Photographer"}
-          onChange={handleChange} />
+          onChange={handleChange}
+        />
         <label>Photographer</label>
         <input
           type="radio"
           name="Persona"
           value="Content Creator"
           checked={prompt.Persona === "Content Creator"}
-          onChange={handleChange} />
+          onChange={handleChange}
+        />
         <label>Content Creator</label>
         <input
           type="radio"
           name="Persona"
           value="Graphic Designer"
           checked={prompt.Persona === "Graphic Designer"}
-          onChange={handleChange} />
+          onChange={handleChange}
+        />
         <label>Graphic Designer</label>
       </div>
       <div
@@ -258,29 +255,35 @@ function App() {
         }}
       >
         {imageSource && (
-          <>
-            <div style={{ display: "flex", alignItems: 'center', justifyContent: 'center' }}>
-              <img style={{ aspectRatio: 'auto', width: '50%' }} src={imageSource} alt="Generated Image" />
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <img
+                style={{ aspectRatio: "auto", width: "50%" }}
+                src={imageSource}
+                alt="Generated Image"
+              />
             </div>
-            <div>Rating</div>
-            <div>
-              <button onClick={onRate} value={1}>
-                1
-              </button>
-              <button onClick={onRate} value={2}>
-                2
-              </button>
-              <button onClick={onRate} value={3}>
-                3
-              </button>
-              <button onClick={onRate} value={4}>
-                4
-              </button>
-              <button onClick={onRate} value={5}>
-                5
-              </button>
+            <div className="rating-section">
+              {Array.from({ length: 10 }, (_, i) => (
+                <button key={i + 1} onClick={onRate} value={i}>
+                  {i + 1}
+                </button>
+              ))}
             </div>
-          </>
+          </div>
         )}
       </div>
     </div>
